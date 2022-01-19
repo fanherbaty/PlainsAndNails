0256a7250352e50f9d4fe11ec9c3930c2eb691ec
fanherbaty <94198186+fanherbaty@users.noreply.github.com>
2022-01-19 23:17:12 +0100

    Delete src directory

--- src/inputStorage.lua
+++
@@ -1,8 +0,0 @@
-input = {}
-input.player = {}
-input.player.walkL = {"a", "left"}
-input.player.walkR = {"d", "right"}
-input.player.walkD = {"s", "down"}
-input.player.walkU = {"w", "up"}
-input.debugToggle = "f1"
-input.player.toggleAimMode = '3' --Coresponding to the mouse
